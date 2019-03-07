ade0a4497855009fe9d46f17661a9ba5bc3f81fd
Thibaud Desodt <thibaud.desodt@telefonica.com>
2019-03-07 12:08:56 +0100

    fix(version): try to keep plugin.xml and package.json in sync

--- publish.sh
+++ publish.sh
@@ -6,6 +6,11 @@ echo "Publishing google-vr-plugin ..."
 if [ -f /.dockerenv ] ; then
     # Increase version
     npm version patch
+    PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+    TO_REPLACE="0.0.0"
+    # update plugin.xml with the same version number as the package.json file
+    sed -i.bak "s/${TO_REPLACE}/${PACKAGE_VERSION}/g" plugin.xml
 fi
 
 npm init -y -scope=alpha-health
